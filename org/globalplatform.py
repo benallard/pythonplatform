0a6978ac83edead0856433b27244d5d0f15f623d
Benoît Allard <benoit.allard@gmx.de>
2011-06-03 16:36:01 +0200

    initial commit: supports getSecureChannel and resetSecurity

---
+++ org/globalplatform.py
@@ -0,0 +1,27 @@
+from pythoncard.framework import *
+
+class SecureChannel(Shareable):
+    AUTHENTICATED = 0x80
+    C_DECRYPTION = 0x02
+    C_MAC = 0x01
+    R_ENCRYPTION = 0x20
+    R_MAC = 0x10
+    NO_SECURITY_LEVEL = 0x00
+
+    def resetSecurity(self):
+        pass
+
+class GPSystem(object):
+    APPLICATION_INSTALLED = 0x03
+    APPLICATION_SELECTABLE = 0x07
+    SECURITY_DOMAIN_PERSONALIZED = 0x0F
+    CARD_OP_READY = 0x01
+    CARD_INITIALIZED = 0x07
+    CARD_SECURED = 0x0F
+    CARD_LOCKED = 0x7F
+    CARD_TERMINATED = 0xFF
+    CVM_GLOBAL_PIN = 0x11
+
+    @staticmethod
+    def getSecureChannel():
+        return SecureChannel()
